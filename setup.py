6158d9fbad00ffbb89cd069749a6aca9830495bd
Toni Colom <toni@semicolom.com>
2018-11-07 20:32:59 +0100

    Fixes installation issues

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-tools-contact',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='A simple Django app to manage company contact information.',
     long_description=README,
