acbda2c2930313a37c77f6998eb64a081cd3af03
Toni Colom <toni@semicolom.com>
2018-11-15 15:31:27 +0100

    Closes #1 adding markdown for long description

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tools-contact',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='A simple Django app to manage company contact information.',
     long_description=README,
+    long_description_content_type="text/markdown",
     author='Toni Colom',
     author_email='toni@semicolom.com',
     install_requires=[
