be360d5dd391577b74a20be6eaa81df2e28766fe
Toni Colom <toni@semicolom.com>
2019-03-29 17:11:13 +0100

    Moves locale folder

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tools-contact',
-    version='1.3.0',
+    version='1.3.1',
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='A simple Django app to manage company contact information.',
